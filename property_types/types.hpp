88fa8cb248360706e2330a2b27c665698a4c22ad
ryanmrichard <ryanmrichard1@gmail.com>
2019-05-07 13:32:56 -0500

    readds TAMM hacks

--- property_types/types.hpp
+++ property_types/types.hpp
@@ -13,20 +13,37 @@
 namespace property_types {
 namespace type {
 
-///Typedef of the class used for tensors
-template<typename T> using tensor = tamm::Tensor<T>;
+/// Typedef of the class used for tensors
+template<typename T>
+using tensor = tamm::Tensor<T>;
 
-///Typedef of the class used to describe a molecule
+/// Typedef of the class used to describe a molecule
 using molecule = LibChemist::Molecule;
 
-///Typedef of the class used to describe an AO basis set
+/// Typedef of the class used to describe an AO basis set
 using basis_set = LibChemist::AOBasisSet;
 
-///Typedef of the class modeling the orbital space
-template<typename T> using orbitals = LibChemist::OrbitalSpace<T>;
+/// Typedef of the class modeling the orbital space
+template<typename T>
+using orbitals = LibChemist::OrbitalSpace<T>;
 
-///Type of a non-negative counting number
+/// Type of a non-negative counting number
 using size = std::size_t;
 
 } // namespace type
-} //namespace property_types
+} // namespace property_types
+
+// These allow SDEAny to wrap a tamm::Tensor, needed for Cache retrieval
+namespace tamm {
+template<typename T, typename U>
+bool operator==(const Tensor<T>& lhs, const Tensor<U>& rhs) {
+    return false;
+}
+
+inline void hash_object(const Tensor<double>& t, SDE::Hasher& h) {
+    std::mt19937 rng;
+    rng.seed(std::random_device()());
+    std::uniform_real_distribution<double> dist;
+    h(dist(rng), dist(rng), dist(rng), dist(rng));
+}
+} // namespace tamm
