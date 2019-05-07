6cf9b7110fe566d79bb014414cf7b20f1a6af39a
ryanmrichard <ryanmrichard1@gmail.com>
2019-05-07 10:50:49 -0500

    renames J/K and factors tests

--- tests/update_mos.cpp
+++ tests/update_mos.cpp
@@ -1,9 +1,8 @@
 #include "test_property_type.hpp"
 #include <property_types/update_mos.hpp>
 
-TEST_CASE("UpdateMOs"){
+TEST_CASE("UpdateMOs") {
     test_property_type<property_types::UpdateMOs<>>(
-        {"Molecule", "Basis Set", "Fock Matrix", "Previous OrbitalSpace"},
-        {"Orbital Space"});
+      {"Molecule", "Basis Set", "Fock Matrix", "Previous OrbitalSpace"},
+      {"Orbital Space"});
 }
-
