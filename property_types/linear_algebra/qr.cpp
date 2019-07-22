55c8f56d2948c1283424e1f4637bbe1bed1cbdfc
David Williams-Young <dbwy@lbl.gov>
2019-07-22 12:20:39 -0400

    Added RankRevealingQRFactorization property type (#9)
    * Added RankRevealingQRFactorization property type
    
    * Added RQR test
    
    * add gitlab yml file
    
    * Update .gitlab-ci.yml

--- property_types/linear_algebra/qr.cpp
+++ property_types/linear_algebra/qr.cpp
@@ -7,4 +7,9 @@ template class QRFactorization<float>;
 template class QRFactorization<std::complex<double>>;
 template class QRFactorization<std::complex<float>>;
 
+template class RankRevealingQRFactorization<double>;
+template class RankRevealingQRFactorization<float>;
+template class RankRevealingQRFactorization<std::complex<double>>;
+template class RankRevealingQRFactorization<std::complex<float>>;
+
 } // namespace property_types
