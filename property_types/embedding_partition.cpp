58ba8be35a1158a3f1b076793461beb5a3ddda25
Jonathan M. Waldrop <relik107@gmail.com>
2019-11-06 14:28:04 -0600

    add embedding_partition type and related pieces

---
+++ property_types/embedding_partition.cpp
@@ -0,0 +1,8 @@
+#include "property_types/embedding_partition.hpp"
+
+namespace property_types {
+
+template class EmbedPartition<double>;
+template class EmbedPartition<float>;
+
+} // namespace property_types
---
+++ tests/embedding_partition.cpp
@@ -0,0 +1,8 @@
+#include "test_property_type.hpp"
+#include <property_types/embedding_partition.hpp>
+
+TEST_CASE("EmbedPartition") {
+    test_property_type<property_types::EmbedPartition<>>(
+        {"Molecule", "Basis Set", "Active Atoms", "Initial OrbitalSpace"},
+        {"N Active MOs", "Orbital Space"});
+}
