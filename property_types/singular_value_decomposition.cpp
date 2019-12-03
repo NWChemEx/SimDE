726d075804d19397bb9f10a6a62d0bddcbeaa8b9
Jonathan M. Waldrop <relik107@gmail.com>
2019-12-03 12:05:38 -0600

    add type for SVD

---
+++ property_types/singular_value_decomposition.cpp
@@ -0,0 +1,8 @@
+#include "property_types/singular_value_decomposition.hpp"
+
+namespace property_types {
+
+    template class SingularValueDecomposition<double>;
+    template class SingularValueDecomposition<float>;
+
+} // namespace property_types
