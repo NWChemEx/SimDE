fb5b7c6c8a2d914aa4ffd37cca9357cbc24f813f
Jonathan M. Waldrop <relik107@gmail.com>
2020-02-20 10:53:51 -0600

    Get rid of old doi property type.

--- property_types/differential_overlap_integral.cpp
+++
@@ -1,8 +0,0 @@
-#include "property_types/differential_overlap_integral.hpp"
-
-namespace property_types {
-
-template class DifferentialOverlapIntegral<double>;
-template class DifferentialOverlapIntegral<float>;
-
-} // namespace property_types
