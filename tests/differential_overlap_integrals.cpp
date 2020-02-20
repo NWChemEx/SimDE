8887e4abb41a7bcc997f385792837d39287f7d5b
Jonathan M. Waldrop <relik107@gmail.com>
2020-02-20 11:57:50 -0600

    Get rid of old doi property type. (#21)

--- tests/differential_overlap_integrals.cpp
+++
@@ -1,8 +0,0 @@
-#include "test_property_type.hpp"
-#include <property_types/differential_overlap_integral.hpp>
-
-TEST_CASE("DifferentialOverlapIntegral") {
-    test_property_type<property_types::DifferentialOverlapIntegral<>>(
-      {"Molecule", "Orbital Space 1", "Orbital Space 2"},
-      {"Differential Overlap Integral"});
-}
