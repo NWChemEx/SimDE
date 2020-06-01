dab8aedf876ceecc3418c3641d0daaf2df9d30cd
Jonathan M. Waldrop <relik107@gmail.com>
2020-06-01 17:06:25 -0500

    update embedding_partition

--- property_types/embedding_partition.hpp
+++ property_types/embedding_partition.hpp
@@ -25,21 +25,21 @@ auto EmbedPartition<ElementType, OrbitalType>::inputs_() {
                     .add_field<const type::molecule&>("Molecule")
                     .add_field<const type::basis_set<ElementType>&>("Basis Set")
                     .template add_field<const std::vector<type::size>&>("Active Atoms")
-                    .template add_field<const OrbitalType&>("Initial OrbitalSpace");
+                    .template add_field<const OrbitalType&>("Initial Orbitals");
     rv["Molecule"].set_description("The molecule associated with the density");
     rv["Basis Set"].set_description("The basis set used for the density");
     rv["Active Atoms"].set_description("The list of active atoms");
-    rv["Initial OrbitalSpace"].set_description("The initial MOs/density");
+    rv["Initial Orbitals"].set_description("The initial orbital space");
     return rv;
 }
 
 template<typename ElementType, typename OrbitalType>
 auto EmbedPartition<ElementType, OrbitalType>::results_() {
     auto rv = sde::declare_result()
-            .add_field<int>("N Active MOs")
-            .template add_field<OrbitalType>("Orbital Space");
-    rv["N Active MOs"].set_description("The number of active MOs");
-    rv["Orbital Space"].set_description("The OrbitalSpace with computed density and MOs");
+            .template add_field<OrbitalType>("Active Orbitals")
+            .template add_field<OrbitalType>("Environment Orbitals");
+    rv["Active Orbitals"].set_description("The active orbital space");
+    rv["Environment Orbitals"].set_description("The environment orbital space");
     return rv;
 }
 
