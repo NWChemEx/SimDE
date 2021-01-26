6cea89c8ffcc11a58d57799df3fbd04d589f5c8c
Ryan Richard <ryanmrichard1@gmail.com>
2021-01-26 14:34:41 -0600

    removes left over TA hash/equality

--- include/property_types/types.hpp
+++ include/property_types/types.hpp
@@ -1,9 +1,7 @@
 #pragma once
-#include <libchemist/basis_set/ao_basis_set.hpp>
 #include <libchemist/molecule/molecule.hpp>
 #include <libchemist/orbital_space/orbital_space.hpp>
 #include <libchemist/types.hpp>
-#include <random>
 #include <sde/types.hpp>
 #include <utilities/containers/case_insensitive_map.hpp>
 
@@ -13,8 +11,7 @@
  *  Property types are encouraged to use these typedefs to stay in sync with
  *  the remainder of the library.
  */
-namespace property_types {
-namespace type {
+namespace property_types::type {
 
 /// Typedef of the class used for tensors
 template<typename T>
@@ -46,22 +43,4 @@ using orbital_map = utilities::CaseInsensitiveMap<T>;
 /// Type of a non-negative counting number
 using size = std::size_t;
 
-} // namespace type
-} // namespace property_types
-
-// These allow sdeAny to wrap a TA::TSpArray, needed for Cache retrieval
-namespace TiledArray {
-template<typename T, typename U>
-bool operator==(const property_types::type::tensor<T>& lhs,
-                const property_types::type::tensor<U>& rhs) {
-    return false;
-}
-
-inline void hash_object(const property_types::type::tensor<double>& t,
-                        sde::type::hasher& h) {
-    std::mt19937 rng;
-    rng.seed(std::random_device()());
-    std::uniform_real_distribution<double> dist;
-    h(dist(rng), dist(rng), dist(rng), dist(rng));
-}
-} // namespace TiledArray
+} // namespace property_types::type
