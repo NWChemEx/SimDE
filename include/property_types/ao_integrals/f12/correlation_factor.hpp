1e4cf7a21972e860810ce459eeaf02dafe5e5456
Ryan Richard <ryanmrichard1@gmail.com>
2021-05-04 13:07:26 -0500

    some tweaks for sparsity

--- include/property_types/ao_integrals/f12/correlation_factor.hpp
+++ include/property_types/ao_integrals/f12/correlation_factor.hpp
@@ -20,13 +20,19 @@ TEMPLATED_PROPERTY_TYPE_INPUTS(CorrelationFactor, BaseType) {
 
 template<typename BaseType>
 TEMPLATED_PROPERTY_TYPE_RESULTS(CorrelationFactor, BaseType) {
-    using element_type = element_t<BaseType>;
-    using tensor_type  = type::tensor<element_type>;
+    using traits       = detail_::NCenterTraits<BaseType>;
+    using element_type = typename traits::element_type;
+    using space_type   = typename traits::space_type;
+    using tensor_type  = typename space_type::overlap_type;
     using my_type      = CorrelationFactor<BaseType>;
 
     return sde::declare_result().add_field<tensor_type>(
       detail_::make_key<my_type>("f_12(r_12)"));
 }
+
+template<typename ElementType>
+using SparseCorrelationFactor4C =
+  CorrelationFactor<SparseFourCenter<ElementType>>;
 
 MULTICENTER_AO_INTEGRAL_TYPEDEFS(CorrelationFactor);
 MULTICENTER_AO_INTEGRAL_EXTERNS(CorrelationFactor);
