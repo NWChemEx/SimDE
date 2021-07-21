cf4eec746666320028a4ff8a45ee4c33973bab97
ryan <ryanmrichard1@gmail.com>
2021-07-21 15:54:13 -0500

    move files around

--- include/property_types/density/update_density.hpp
+++ include/property_types/density/update_density.hpp
@@ -1,3 +1,20 @@
 #pragma once
 
-namespace simde {}
+namespace simde {
+
+DECLARE_PROPERTY_TYPE(UpdateDensity);
+
+PROPERTY_TYPE_INPUTS(UpdateDensity) {
+    using tensor_type = const type::tensor&;
+
+    auto rv = sde::declare_inputs()
+                .add_field<tensor_type>("Fock Matrix")
+                .template add_field<tensor_type>("Density");
+    return rv;
+}
+
+PROPERTY_TYPE_RESULTS(UpdateDensity) {
+    return sde::declare_results().add_field<type::tensor>("New density");
+}
+
+} // namespace simde
