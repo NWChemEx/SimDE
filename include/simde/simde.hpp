425ad5a57019899ca70c438b748215b1023f984d
Ryan Richard <ryanmrichard1@gmail.com>
2021-08-19 12:04:54 -0500

    changes for mp2 (#63)

--- include/simde/simde.hpp
+++ include/simde/simde.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include "simde/density/density.hpp"
 #include "simde/energy/energy.hpp"
+#include "simde/energy_denominator.hpp"
 #include "simde/operators/operators.hpp"
 #include "simde/tensor_representation/tensor_representation.hpp"
 #include "simde/wavefunctions/wavefunctions.hpp"
