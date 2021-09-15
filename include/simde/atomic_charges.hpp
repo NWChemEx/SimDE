825e8b87fcde9c2cf87d2fe85c8b7f73f9affd34
Jonathan M. Waldrop <relik107@gmail.com>
2021-09-15 17:03:27 -0500

    update atomic charges ptype

--- include/simde/atomic_charges.hpp
+++ include/simde/atomic_charges.hpp
@@ -6,33 +6,24 @@ namespace simde {
 
 /**
  * @brief The property type for modules that calculate atomic charges.
- *
- * @tparam ElementType The type of the elements in the returned tensor
- * @tparam OrbitalType The type of the input orbital space
  */
-template<typename OrbitalType>
-DECLARE_TEMPLATED_PROPERTY_TYPE(AtomicCharges, OrbitalType);
+DECLARE_PROPERTY_TYPE(AtomicCharges);
 
 //---------------------------Implementations------------------------------------
-template<typename OrbitalType>
-TEMPLATED_PROPERTY_TYPE_INPUTS(AtomicCharges, OrbitalType) {
+PROPERTY_TYPE_INPUTS(AtomicCharges) {
     auto rv = pluginplay::declare_input()
                 .add_field<const type::molecule&>("Molecule")
-                .add_field<const OrbitalType&>("Molecular Orbitals");
+                .add_field<const type::el_density &>("Density");
     rv["Molecule"].set_description("The molecular system");
-    rv["Molecular Orbitals"].set_description("The molecular orbitals");
+    rv["Density"].set_description("The electronic density");
     return rv;
 }
 
-template<typename OrbitalType>
-TEMPLATED_PROPERTY_TYPE_RESULTS(AtomicCharges, OrbitalType) {
+PROPERTY_TYPE_RESULTS(AtomicCharges) {
     auto rv =
       pluginplay::declare_result().add_field<type::tensor>("Partial Charges");
     rv["Partial Charges"].set_description("The calculated partial charges");
     return rv;
 }
-
-extern template class AtomicCharges<type::derived_space>;
-extern template class AtomicCharges<type::canonical_space>;
 
 } // namespace simde
