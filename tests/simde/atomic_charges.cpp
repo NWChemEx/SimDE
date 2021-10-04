39e18edbce487da8c382e27adabe5a60a918dc29
Jonathan M. Waldrop <relik107@gmail.com>
2021-10-04 12:50:31 -0500

    fix test

--- tests/simde/atomic_charges.cpp
+++ tests/simde/atomic_charges.cpp
@@ -1,7 +1,7 @@
 #include "simde/atomic_charges.hpp"
 #include "test_property_type.hpp"
 
-TEMPLATE_TEST_CASE("AtomicCharges", "", float, double) {
-    test_property_type<simde::AtomicCharges<TestType>>(
-      {"Molecule", "Molecular Orbitals"}, {"Partial Charges"});
+TEST_CASE("AtomicCharges") {
+    test_property_type<simde::AtomicCharges>(
+      {"Molecule", "Density"}, {"Partial Charges"});
 }
