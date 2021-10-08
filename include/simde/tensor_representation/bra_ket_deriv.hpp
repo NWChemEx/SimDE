c46058ebe8d9a04cb4220d59544034960de359d5
Clang Robot <robot@example.com>
2021-10-08 18:40:57 +0000

    Committing clang-format changes

--- include/simde/tensor_representation/bra_ket_deriv.hpp
+++ include/simde/tensor_representation/bra_ket_deriv.hpp
@@ -20,14 +20,17 @@ TEMPLATED_PROPERTY_TYPE_INPUTS(BraKetDeriv, BraType, OpType, KetType) {
 
 template<typename BraType, typename OpType, typename KetType>
 TEMPLATED_PROPERTY_TYPE_RESULTS(BraKetDeriv, BraType, OpType, KetType) {
-    auto rv = pluginplay::declare_result().add_field<std::vector<libchemist::Point<double>>>("gradient");
+    auto rv = pluginplay::declare_result()
+                .add_field<std::vector<libchemist::Point<double>>>("gradient");
     return rv;
 }
 
 template<typename BraType>
-using ElectronicEnergy_Nuclear = BraKetDeriv<BraType, type::els_hamiltonian_nuc, BraType>;
+using ElectronicEnergy_Nuclear =
+  BraKetDeriv<BraType, type::els_hamiltonian_nuc, BraType>;
 
 template<typename BraType>
-using TotalEnergy_Nuclear = BraKetDeriv<BraType, type::hamiltonian_nuc, BraType>;
+using TotalEnergy_Nuclear =
+  BraKetDeriv<BraType, type::hamiltonian_nuc, BraType>;
 
 } // namespace simde
--- include/simde/tensor_representation/tensor_representation.hpp
+++ include/simde/tensor_representation/tensor_representation.hpp
@@ -17,14 +17,16 @@ using space_map_t = typename detail_::TensorRepTraits<T>::map_type;
 
 using TotalCanonicalEnergy = TotalEnergy<type::canonical_reference>;
 
-using TotalCanonicalEnergy_Nuclear = TotalEnergy_Nuclear<type::canonical_reference>;
+using TotalCanonicalEnergy_Nuclear =
+  TotalEnergy_Nuclear<type::canonical_reference>;
 
 using WfEnergy [[deprecated("Use TotalCanonicalEnergy instead")]] =
   TotalCanonicalEnergy;
 
 using CanonicalElectronicEnergy = ElectronicEnergy<type::canonical_reference>;
 
-using CanonicalElectronicEnergy_Nuclear = ElectronicEnergy_Nuclear<type::canonical_reference>;
+using CanonicalElectronicEnergy_Nuclear =
+  ElectronicEnergy_Nuclear<type::canonical_reference>;
 
 using CanonicalCorrelationEnergy =
   CorrelationEnergy<type::canonical_reference, type::canonical_many_body>;
