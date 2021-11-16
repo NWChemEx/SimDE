b6dfb29f3b832cc1e45190ce862c39ac7aa37b2e
Jonathan M. Waldrop <relik107@gmail.com>
2021-11-16 13:49:37 -0600

    remove derivative from Cauchy-Schwarz

--- include/simde/cauchy_schwarz_approximation.hpp
+++ include/simde/cauchy_schwarz_approximation.hpp
@@ -12,12 +12,9 @@ PROPERTY_TYPE_INPUTS(ShellNorms) {
     using basis_type = const simde::type::ao_space&;
     auto rv          = pluginplay::declare_input()
                 .add_field<basis_type>("Basis1")
-                .template add_field<basis_type>("Basis2")
-                .template add_field<type::size>("Derivative", type::size{0});
+                .template add_field<basis_type>("Basis2");
     rv["Basis1"].set_description("The first basis set");
     rv["Basis2"].set_description("The second basis set");
-    rv["Derivative"].set_description(
-      "The derivative order of integrals to be computed");
     return rv;
 }
 
