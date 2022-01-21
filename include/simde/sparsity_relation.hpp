77c90e77851c4ef10e0772788775308b72515d20
Jonathan M. Waldrop <relik107@gmail.com>
2022-01-21 15:13:18 -0600

    Tensorwrapper relocation (#86)
    * fix sparse_map typedef
    
    * fix return type in sparsity relation ptype

--- include/simde/sparsity_relation.hpp
+++ include/simde/sparsity_relation.hpp
@@ -24,8 +24,8 @@ TEMPLATED_PROPERTY_TYPE_INPUTS(SparsityRelation, IndependType, DependType) {
 
 template<typename IndependType, typename DependType>
 TEMPLATED_PROPERTY_TYPE_RESULTS(SparsityRelation, IndependType, DependType) {
-    using chemist::sparse_map::SparseMapEE;
-    auto rv = pluginplay::declare_result().add_field<SparseMapEE>("Sparse Map");
+    using chemist::type::sparse_map;
+    auto rv = pluginplay::declare_result().add_field<sparse_map>("Sparse Map");
     rv["Sparse Map"].set_description(
       "Map from a given member of the independent basis to its"
       " domain in the dependent basis set");
