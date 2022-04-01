5159537212d0adb0a109cdd27e715b93493272cc
ryan <ryanmrichard1@gmail.com>
2022-04-01 11:55:50 -0500

    Adds AtomicOrbital PT

--- include/simde/orbital_spaces/orbital_spaces.hpp
+++ include/simde/orbital_spaces/orbital_spaces.hpp
@@ -1,4 +1,5 @@
 #pragma once
+#include "simde/orbital_spaces/atomic_orbitals.hpp"
 #include "simde/orbital_spaces/auxiliary_basis_set.hpp"
 #include "simde/orbital_spaces/complimentary_auxiliary_basis_set.hpp"
 #include "simde/orbital_spaces/density_fit_basis.hpp"
