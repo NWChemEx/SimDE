9f54b1b2f2ccb3b8089fadaa4442ff9c049fd367
Clang Robot <robot@example.com>
2022-06-30 21:44:07 +0000

    Committing clang-format changes

--- tests/simde/cauchy_schwarz_approximation.cpp
+++ tests/simde/cauchy_schwarz_approximation.cpp
@@ -4,6 +4,6 @@
 TEST_CASE("CauchySchwarz") {
     using op_t = simde::type::el_el_coulomb;
     op_t op;
-    test_property_type<simde::ShellNorms<op_t>>(
-      {"bra", op.as_string() ,"ket"}, {"Screening Matrix"});
+    test_property_type<simde::ShellNorms<op_t>>({"bra", op.as_string(), "ket"},
+                                                {"Screening Matrix"});
 }
