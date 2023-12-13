67ac77066bc237be9c97f6c08baf83efac742edc
Murat Keceli <keceli@gmail.com>
2023-12-13 02:06:14 -0600

    Add optimize pts

---
+++ include/simde/optimize/nuclear.hpp
@@ -0,0 +1,35 @@
+/*
+ * Copyright 2022 NWChemEx-Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <simde/energy/ao_energy.hpp>
+#include <simde/optimize/optimize_pt.hpp>
+#include <simde/types.hpp>
+
+namespace simde {
+
+/** @brief Type def of Optimize for when we want the optimize
+ *         with respect to nuclear coordinates.
+ *
+ *  @tparam T The property type we are optimizing for.
+ */
+template<typename T>
+using NuclearOptimize = Optimize<T, type::molecule>;
+
+/// Type for the optimization of an AOEnergy w.r.t. nuclear coordinates.
+using OptimizeCoordinates = NuclearOptimize<AOEnergy>;
+
+} // namespace simde
---
+++ include/simde/optimize/optimize_pt.hpp
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2022 NWChemEx-Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#pragma once
+#include <pluginplay/pluginplay.hpp>
+#include <simde/types.hpp>
+
+namespace simde {
+
+/** @brief Declares the Optimize property type.
+ *
+ *  The Optimize template property type is templated on the quantity we are
+ *  taking the Optimize of and the type we are taking the Optimize with
+ *  respect to. For example:
+ *
+ *  ```.cpp
+ *  Optimize<AOEnergy, Molecule>;
+ *  ```
+ *
+ *  is the property type for a module which computes the optimized
+ *  AOEnergy with respect to nuclear coordinates.
+ *
+ *
+ *  @tparam PT2Optimize The unqualified property type being differentiated.
+ *  @tparam WithRespectTo The unqualified type we are taking the Optimize with
+ *                        respect to.
+ */
+template<typename PT2Optimize, typename WithRespectTo>
+DECLARE_TEMPLATED_PROPERTY_TYPE(Optimize, PT2Optimize, WithRespectTo);
+
+template<typename PT2Optimize, typename WithRespectTo>
+TEMPLATED_PROPERTY_TYPE_INPUTS(Optimize, PT2Optimize, WithRespectTo) {
+    auto label = "To be optimized";
+    return PT2Optimize::inputs().template add_field<WithRespectTo>(label);
+}
+
+template<typename PT2Optimize, typename WithRespectTo>
+TEMPLATED_PROPERTY_TYPE_RESULTS(Optimize, PT2Optimize, WithRespectTo) {
+    return pluginplay::declare_result().template add_field<WithRespectTo>(
+      "Optimized");
+}
+
+} // namespace simde
