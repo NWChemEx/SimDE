11fa3ff37ee526554389fa28a74ec3ed93e6fe8f
jwaldrop107 <jwaldrop107@users.noreply.github.com>
2024-05-07 18:45:42 +0000

    deploy: a9c5c04c342c356d6afc0bb09c030bc3bf9d2a81

--- simde_cxx_api/search/all_0.js
+++ simde_cxx_api/search/all_0.js
@@ -3,6 +3,6 @@ var searchData=
   ['ao_5fbasis_5fset_0',['ao_basis_set',['../types_8hpp.html#a9a38b93106424a406c2e496a4685a554',1,'simde::type']]],
   ['atom_1',['atom',['../types_8hpp.html#adb3f4f81ddbf80d35ffe3640271d6d59',1,'simde::type']]],
   ['atomic_5fbasis_5fset_2',['atomic_basis_set',['../types_8hpp.html#a444827d911af1008df53b663916ca50d',1,'simde::type']]],
-  ['atomic_5fnumber_3',['atomic_number',['../types_8hpp.html#a14c76018005aaa9fabd34390ce3f4034',1,'simde::type']]],
+  ['atomic_5fnumber_3',['atomic_number',['../types_8hpp.html#ae97e6857b370b08ec21b7a4f848c406a',1,'simde::type']]],
   ['atomic_5fsymbol_4',['atomic_symbol',['../types_8hpp.html#af1129fa0d7f0f6e5854794f80290e464',1,'simde::type']]]
 ];
