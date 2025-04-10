fcdf33b8fdbc8b6a8e76c84a82ba1f6c5f7b4991
ryanmrichard <ryanmrichard@users.noreply.github.com>
2025-04-10 16:07:27 +0000

    deploy: 661a594564d7663a5a4cbabbac8ea81d42496645

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,5 +1,5 @@
 const DOCUMENTATION_OPTIONS = {
-    VERSION: 'v0.0.50',
+    VERSION: 'v0.0.51',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
--- simde_cxx_api/search/all_0.js
+++ simde_cxx_api/search/all_0.js
@@ -2,7 +2,7 @@ var searchData=
 [
   ['ao_5fbasis_5fset_0',['ao_basis_set',['../types_8hpp.html#a9a38b93106424a406c2e496a4685a554',1,'simde::type']]],
   ['aos_1',['aos',['../types_8hpp.html#a7fc0faac435bbc78be5169a390e31cf2',1,'simde::type']]],
-  ['aos_5fsquared_2',['aos_squared',['../types_8hpp.html#a2825931df0758641de27c6d84c1afc9d',1,'simde::type']]],
+  ['aos_5fsquared_2',['aos_squared',['../types_8hpp.html#aac4a6580870a2c311f9eadb665cc5e51',1,'simde::type']]],
   ['atom_3',['atom',['../types_8hpp.html#afc9860f58ea184eb590380f3b762ca70',1,'simde::type']]],
   ['atomic_5fbasis_5fset_4',['atomic_basis_set',['../types_8hpp.html#a444827d911af1008df53b663916ca50d',1,'simde::type']]],
   ['atomic_5fnumber_5',['atomic_number',['../types_8hpp.html#ae97e6857b370b08ec21b7a4f848c406a',1,'simde::type']]],
